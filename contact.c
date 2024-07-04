4de7b56fa13fac853ad76fa562f6646604f014ef
srikanthpragada <srikanthpragada@gmail.com>
2024-07-04 16:30:34 +0530

    04-JUL

---
+++ contact.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+
+void main()
+{
+     printf("contact@srikanthtechnologies.com\n");
+     printf("9059057000");
+}
---
+++ square.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+void main()
+{
+     int num, square;   // variable
+
+       num = 25;
+       square = num * num;
+
+       printf("Square of %d is %d", num, square);
+}
